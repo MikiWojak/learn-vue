e5cbb3585220978b597f28fe7ff8044c34d20510
MikiWojak <mikizarowa8@gmail.com>
2021-03-09 18:47:30 +0100

    Message Component - WIP
    - Add the component
    - Working on the close button

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 Vue.config.devtools = true;
 
-Vue.component('task-list', {
+Vue.component('message', {
+  props: ['title', 'body'],
+
   template: `
-    <div>
-      <task v-for="task in tasks">{{ task.description }}</task>
-    </div>`,
-
-  data() {
-    return {
-      tasks: [
-        { description: 'Go to the store', completed: true },
-        { description: 'Go to the email', completed: false },
-        { description: 'Go to the farm', completed: true },
-        { description: 'Go to work', completed: false }
-      ]
-    }
-  }
-});
-
-Vue.component('task', {
-  template: '<li><slot></slot></li>'
+  <article class="message">
+    <div class="message-header">
+      {{ title }}
+      <button type="button">x</button>
+    </div>
+    <div class="message-body">
+      {{ body }}
+    </div>
+  </article>
+  `
 });
 
 new Vue({
