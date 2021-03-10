dd636988ec17777ee27e42ab1c7670fd0f4557ab
MikiWojak <mikizarowa8@gmail.com>
2021-03-10 16:45:33 +0100

    Message Component completed
    - Finish the component
    - Add button to hide the component

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ Vue.config.devtools = true;
 Vue.component('message', {
   props: ['title', 'body'],
 
+  data() {
+    return {
+      isVisible: true
+    }
+  },
+
   template: `
-  <article class="message">
+  <article class="message" v-show="isVisible">
     <div class="message-header">
       {{ title }}
-      <button type="button">x</button>
+      <button type="button" @click="isVisible = false">x</button>
     </div>
     <div class="message-body">
       {{ body }}
     </div>
   </article>
-  `
+  `,
 });
 
 new Vue({
