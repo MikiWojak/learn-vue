5e0b6294ce533e78c43c730b67afba1172e59308
MikiWojak <mikizarowa8@gmail.com>
2021-03-12 19:18:31 +0100

    Tabs Components - Content
    - Code changing content of the tab
    - Code changing address while clicking on the tab

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Vue.component('tabs', {
     <div class="tabs">
       <ul>
         <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
-          <a href="#" @click="selectTab(tab)">{{ tab.name }}</a>
+          <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
         </li>
       </ul>
     </div>
@@ -37,7 +37,7 @@ Vue.component('tabs', {
 
 Vue.component('tab', {
   template: `
-    <div><slot></slot></div>
+    <div v-show="isActive"><slot></slot></div>
   `,
 
   props: {
@@ -48,6 +48,12 @@ Vue.component('tab', {
   data() {
     return {
       isActive: false
+    }
+  },
+
+  computed: {
+    href() {
+      return '#' + this.name.toLowerCase().replace(/ /g, '-');
     }
   },
 
